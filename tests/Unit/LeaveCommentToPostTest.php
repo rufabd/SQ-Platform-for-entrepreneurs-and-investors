dc41404560cbfccff1e5c2710e87917294f72cc0
rufat <mrrufatabd@gmail.com>
2022-10-23 15:31:41 +0300

    1st cycle - [Test LeaveCommentToPostTest] - Test was created and failed

---
+++ tests/Unit/LeaveCommentToPostTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class LeaveCommentToPostTest extends TestCase
+{
+    use WithoutMiddleware;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_comment_can_be_left_to_the_post()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->call('POST', 'comments', [
+            'user_id' => 21,
+            'project_post_id' => 1,
+            'body' => 'This is new comment added'
+        ]);
+        // dd($response);
+        // dd($response->assertStatus(200));
+        $data = json_decode($response->getContent(), true);
+        if($data['status'] == 'success') {
+            // dd($data['comment']);
+            $this->assertTrue(true);
+            $response->assertStatus(200);
+            // $response->assertSee($data['body']);
+        } else {
+            $this->assertTrue(false);
+        }                                 
+    }
+}
