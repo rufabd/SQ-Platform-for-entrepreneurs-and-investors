d866ea77e98b08eda89af1d02634e4543343fc6e
rufat <mrrufatabd@gmail.com>
2022-10-23 15:18:42 +0300

    1st cycle - [Test SearchPostTest] - Test was added and failed

---
+++ tests/Unit/SearchPostTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+use function PHPUnit\Framework\assertEquals;
+
+class SearchPostTest extends TestCase
+{
+    use WithoutMiddleware;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_search_project_post_by_title()
+    {
+        $searchResponse = $this->call('GET', '/founder/project-posts?search=soft');
+        $data = json_decode($searchResponse->getContent(), true);
+        if($data['status'] == 'success') {
+            // dd($data['title']);
+            // dd($data['status']);
+            $this->assertTrue(true);
+            $searchResponse->assertSee($data['title']);
+        } else {
+            // dd($data['status']);
+            $this->assertTrue(false);
+            $searchResponse->assertDontSee($data['title']);
+        }
+        
+    }
+}
